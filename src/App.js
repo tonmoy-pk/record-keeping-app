4ec785a25983bef7b85b8536e704b7522f28ba8c
tonmoy-pk <tonmoykumar78@gmail.com>
2023-02-16 00:09:25 +0600

    textfield added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Haeader from './components/Haeader';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
 
 function App() {
+
+  const [date, setDate] = useState("");
+  const [target, setTarget] = useState("");
+  const [data, setData] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Haeader/>
+      <div className='form'>
+        <Stack spacing={2} direction="row">
+          <TextField value={date} onChange={(e) => setDate(e.target.value)} id="outlined-basic" label="Date" variant="outlined" />
+          <TextField value={target} onChange={(e) => setTarget(e.target.value)} id="outlined-basic" label="Target" variant="outlined" />
+          <Button variant="contained"><AddIcon/></Button>
+        </Stack>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/Haeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Haeader() {
+  return (
+    <div className='header'>
+        <h1>TRACK RECORDS</h1>
+     
+    </div>
+  )
+}
+
+export default Haeader
